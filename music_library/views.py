970a3e38d7d7fd3e72e24af7c5eec84b5278c9d3
Fuechee <f9vang@gmail.com>
2022-10-29 16:09:47 -0500

    Created API GET, POST for getting all songs

--- music_library/views.py
+++ music_library/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
 
 # Create your views here.
+@api_view(['GET','POST'])
+def songs(request):
+    if request.method == 'GET':
+        query_set = Song.objects.all()
+        serailizer = SongSerializer(query_set, many=True)
+        return Response(serailizer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serailizer = SongSerializer(data=request.data)
+        serailizer.is_valid(raise_exception=True)
+        serailizer.save()
+        return Response(serailizer.data, status=status.HTTP_201_CREATED)
